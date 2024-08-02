c292fd547c0b7c0b46d6b712d6ec9437914eb124
Direna88 <131954428+Direna88@users.noreply.github.com>
2024-08-02 17:21:35 +0200

    Adding the other fields of the profile user

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -6,6 +6,11 @@ import {useState, useEffect} from "react";
 export default function ProfilePage() {
   const session = useSession();
   const [userName, setUserName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [streetAddress, setStreetAddress] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
   const {status} = session;
 
   useEffect(() => {
@@ -46,9 +51,55 @@ export default function ProfilePage() {
         </h2>
         <div className="items-center">
           <form className="grow" onSubmit={handleProfileInfoUpdate}>
-            <input type="text" placeholder="First and last name"
+            <label>
+              First and Last Name
+            </label>
+            <input
+              type="text"
+              placeholder="First and last name"
               value={userName} onChange={ev => setUserName(ev.target.value)}/>
-            {/*<input type="email" disabled={true} value={session.data.user.email}/>*/}
+
+            <label>Email</label>
+            {/*<input type="email"
+            disabled={true}*/}
+            {/*value={session.data.user.email}*/}
+            {/*placeholder={'email'}*/}
+            {/*/>*/}
+
+            <label>Phone</label>
+            <input
+              type="tel"
+              placeholder="Phone Number"
+              value={phone} onChange={ev => setPhone(ev.target.value)}/>
+
+            <label>Street Address</label>
+            <input
+              type="text"
+              placeholder="Street Address"
+              value={streetAddress} onChange={ev => setStreetAddress(ev.target.value)}/>
+
+            <div className="flex gap-2">
+              <div>
+                <label>Postal Code</label>
+                <input
+                  type="text"
+                  placeholder="Postal code"
+                  value={postalCode} onChange={ev => setPostalCode(ev.target.value)}/>
+              </div>
+              <div>
+                <label>City</label>
+                <input
+                  type="text"
+                  placeholder="City"
+                  value={city} onChange={ev => setCity(ev.target.value)}/>
+              </div>
+            </div>
+
+            <label>Country</label>
+            <input
+              type="text"
+              placeholder="Country"
+              value={country} onChange={ev => setCountry(ev.target.value)}/>
             <button type="submit">Save</button>
           </form>
         </div>
